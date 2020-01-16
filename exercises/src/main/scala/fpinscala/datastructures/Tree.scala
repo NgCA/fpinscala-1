4ed699bcedbb6396a3095d847233c8a19742f2dc
Alex Ng <alex.ng@showtime.net>
2020-01-16 12:07:52 -0500

    exercise 3.25

--- exercises/src/main/scala/fpinscala/datastructures/Tree.scala
+++ exercises/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -6,8 +6,14 @@ case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 
 
 object Tree {
-
-
-
-
+  /*
+  Exercise 3.25
+  Write a function size that counts the number of nodes (leaves and branches) in a tree.
+   */
+  def size[A](tree: Tree[A]) : Int = {
+    tree match {
+      case Leaf(_) => 1
+      case Branch(left, right) => 1 + size(left) + size(right)
+    }
+  }
 }
