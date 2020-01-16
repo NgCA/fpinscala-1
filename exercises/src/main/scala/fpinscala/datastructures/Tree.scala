b18bd040ac3a3fd6c0ad406276822b628371ef56
Alex Ng <alex.ng@showtime.net>
2020-01-16 12:08:12 -0500

    exercise 3.26

--- exercises/src/main/scala/fpinscala/datastructures/Tree.scala
+++ exercises/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -16,4 +16,16 @@ object Tree {
       case Branch(left, right) => 1 + size(left) + size(right)
     }
   }
+
+  /*
+  Exercise 3.26
+  Write a function maximum that returns the maximum element in a Tree[Int].
+  (Note: In Scala, you can use x.max(y) or x max y to compute the maximum of two integers x andy.)
+   */
+  def maximum(tree: Tree[Int]) : Int = {
+    tree match {
+      case Leaf(value) => value
+      case Branch(l, r) => maximum(l) max maximum(r)
+    }
+  }
 }
