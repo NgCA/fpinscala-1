e50833d9e7004523d3bb2e29e5b67908687b2690
Alex Ng <alex.ng@showtime.net>
2020-01-23 11:54:49 -0500

    exercise 3.28

--- exercises/src/main/scala/fpinscala/datastructures/Tree.scala
+++ exercises/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -39,4 +39,15 @@ object Tree {
       case Branch(l, r) => 1 + (maxDepth(l) max maxDepth(r))
     }
   }
+  /*
+  Exercise 3.28
+  Write a function map, analogous to the method of the same name on List,
+  that modifies each element in a tree with a given function.
+   */
+  def map[A,B](tree: Tree[A])(f: A => B) : Tree[B] = {
+    tree match {
+      case Leaf(v) => Leaf(f(v))
+      case Branch(l, r) => Branch(map(l)(f), map(r)(f))
+    }
+  }
 }
