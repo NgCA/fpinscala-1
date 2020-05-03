d4cfb05f676a004a6d647e3c172e4c01c18ad560
Alex Ng <alex.ng@showtime.net>
2020-05-03 18:55:06 -0400

    exercise 5.2 and formatting

--- exercises/src/main/scala/fpinscala/laziness/Stream.scala
+++ exercises/src/main/scala/fpinscala/laziness/Stream.scala
@@ -1,15 +1,18 @@
 package fpinscala.laziness
 
 import Stream._
+
+import scala.annotation.tailrec
+
 trait Stream[+A] {
 
   def foldRight[B](z: => B)(f: (A, => B) => B): B = // The arrow `=>` in front of the argument type `B` means that the function `f` takes its second argument by name and may choose not to evaluate it.
     this match {
-      case Cons(h,t) => f(h(), t().foldRight(z)(f)) // If `f` doesn't evaluate its second argument, the recursion never occurs.
+      case Cons(h, t) => f(h(), t().foldRight(z)(f)) // If `f` doesn't evaluate its second argument, the recursion never occurs.
       case _ => z
     }
 
-  def exists(p: A => Boolean): Boolean = 
+  def exists(p: A => Boolean): Boolean =
     foldRight(false)((a, b) => p(a) || b) // Here `b` is the unevaluated recursive step that folds the tail of the stream. If `p(a)` returns `true`, `b` will never be evaluated and the computation terminates early.
 
   @annotation.tailrec
@@ -17,9 +20,37 @@ trait Stream[+A] {
     case Empty => None
     case Cons(h, t) => if (f(h())) Some(h()) else t().find(f)
   }
-  def take(n: Int): Stream[A] = ???
 
-  def drop(n: Int): Stream[A] = ???
+  /*
+  Exercise 5.2
+  implement take and drop
+   */
+  def take(n: Int): Stream[A] = {
+    n match {
+      case a if a < 0 => sys.error("n must be greater than or equal to 0")
+      case 0 => Empty
+      case _ => {
+        this match {
+          case Empty => Empty
+          case Cons(h, t) => cons(h(), t().take(n - 1))
+        }
+      }
+    }
+  }
+
+  @tailrec
+  final def drop(n: Int): Stream[A] = {
+    n match {
+      case a if a < 0 => sys.error("n must be greater than or equal to 0")
+      case 0 => this
+      case _ => {
+        this match {
+          case Empty => Empty
+          case Cons(_, t) => t().drop(n - 1)
+        }
+      }
+    }
+  }
 
   def takeWhile(p: A => Boolean): Stream[A] = ???
 
@@ -50,7 +81,9 @@ trait Stream[+A] {
 
   }
 }
+
 case object Empty extends Stream[Nothing]
+
 case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
 
 object Stream {
@@ -63,10 +96,11 @@ object Stream {
   def empty[A]: Stream[A] = Empty
 
   def apply[A](as: A*): Stream[A] =
-    if (as.isEmpty) empty 
+    if (as.isEmpty) empty
     else cons(as.head, apply(as.tail: _*))
 
   val ones: Stream[Int] = Stream.cons(1, ones)
+
   def from(n: Int): Stream[Int] = ???
 
   def unfold[A, S](z: S)(f: S => Option[(A, S)]): Stream[A] = ???
