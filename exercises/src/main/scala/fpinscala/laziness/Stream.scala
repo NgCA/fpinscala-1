c9672d9e8d25d96fdabf55d9d93314a4d5467bd1
Alex Ng <alex_c_ng@yahoo.com>
2020-06-13 22:54:17 -0400

    exercise 5.11

--- exercises/src/main/scala/fpinscala/laziness/Stream.scala
+++ exercises/src/main/scala/fpinscala/laziness/Stream.scala
@@ -173,5 +173,17 @@ object Stream {
     innerFib()
   }
 
-  def unfold[A, S](z: S)(f: S => Option[(A, S)]): Stream[A] = ???
+  /*
+  Exercise 5.11
+  Write a more general stream-building function called unfold.
+  It takes an initial state, and a function for producing both the next state
+  and the next value in the generated stream.
+   */
+  def unfold[A, S](z: S)(f: S => Option[(A, S)]): Stream[A] = {
+    //followed types to implementation, section in book mentioned Option is used to terminate
+    f(z) match {
+      case None => empty
+      case Some((nextValue, nextState)) => cons(nextValue, unfold(nextState)(f))
+    }
+  }
 }
--- exercises/src/test/scala/fpinscala/laziness/StreamTest.scala
+++ exercises/src/test/scala/fpinscala/laziness/StreamTest.scala
@@ -5,7 +5,14 @@ class StreamTest extends org.scalatest.FunSuite {
   test("5.10 fibonnaci") {
     val fib = Stream.fibs()
     val actual = fib.take(6).toList
-    val expected = List(0,1,1,2,3,5)
+    val expected = List(0, 1, 1, 2, 3, 5)
+    assert(actual == expected)
+  }
+
+  test("5.11 unfold") {
+    val unfolded = Stream.unfold(0)(s => if (s % 5 == 0) Some(s + 5, s + 5) else None)
+    val actual = unfolded.take(5).toList
+    val expected = List(5, 10, 15, 20, 25)
     assert(actual == expected)
   }
 }
