0315c7f563e3457d3a95bf8216928daacf352092
Alex Ng <alex_c_ng@yahoo.com>
2020-06-21 21:03:08 -0400

    exercise 6.6

--- exercises/src/main/scala/fpinscala/state/State.scala
+++ exercises/src/main/scala/fpinscala/state/State.scala
@@ -119,7 +119,18 @@ object RNG {
     //can also use _ instead of i => i
   }
 
-  def map2[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = ???
+  /*
+  Exercise 6.6
+  Write the implementation of map2 based on the following signature. This function takes two actions, ra and rb, and
+  a function f for combining their results, and returns a new action that combines them
+   */
+  def map2[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = {
+    rng => {
+      val (a, rng1) = ra(rng)
+      val (b, rng2) = rb(rng1)
+      (f(a,b), rng2)
+    }
+  }
 
   def sequence[A](fs: List[Rand[A]]): Rand[List[A]] = ???
 
