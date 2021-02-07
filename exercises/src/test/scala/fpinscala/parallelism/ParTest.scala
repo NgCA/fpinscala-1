205e188eeec58605e361bbff657c58c06d29d663
Alex Ng <alex_c_ng@yahoo.com>
2021-02-07 13:25:51 -0500

    exercise 7.5

--- exercises/src/test/scala/fpinscala/parallelism/ParTest.scala
+++ exercises/src/test/scala/fpinscala/parallelism/ParTest.scala
@@ -1,5 +1,6 @@
 package fpinscala.parallelism
 
+import fpinscala.parallelism.Par.Par
 import org.scalatest.funsuite.AnyFunSuite
 
 import java.util.concurrent.{Executors, TimeUnit, TimeoutException}
@@ -22,6 +23,17 @@ class ParTest extends AnyFunSuite {
     val secondPar = Par.unit(2)
     val actual = Par.map2WithTimeout(firstPar, secondPar, 0, TimeUnit.MILLISECONDS)(_ + _)
     assertThrows[TimeoutException](actual(es).get)
+  }
+
+  test("sequence") {
+    val input: List[Par[Int]] = List(
+      Par.unit(1),
+      Par.unit(2)
+    )
+
+    val actual: Par[List[Int]] = Par.sequence(input)
+    val expected = List(1,2)
+    assert(actual(es).get == expected)
   }
 
 }
